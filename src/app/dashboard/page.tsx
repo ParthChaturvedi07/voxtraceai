6fa3d4d43d92a3893ab710afcec26017bc8ce3bb
Parth Chaturvedi <parthchaturvedi0204@gmail.com>
2025-12-12 01:15:45 +0530

    08: Trpc Setup

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,4 +1,21 @@
-const Page = async () => {
-  return <div className="text-white">Dashboard Page</div>;
+"use client";
+
+import { useTRPC } from "@/trpc/client";
+import { useQuery } from "@tanstack/react-query";
+
+const Page = () => {
+  const trpc = useTRPC();
+  const { data, isLoading, error } = useQuery(trpc.hello.queryOptions({ text: "Parth" }));
+
+  if (isLoading) {
+    return <div className="text-white">Loading...</div>;
+  }
+
+  if (error) {
+    return <div className="text-white">Error: {error.message}</div>;
+  }
+
+  return <div className="text-white">{data?.greeting}</div>;
 };
+
 export default Page;
