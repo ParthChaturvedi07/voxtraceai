f6b886625610a69d4abfe660b962a811a96b5e88
Parth Chaturvedi <parthchaturvedi0204@gmail.com>
2025-12-22 01:15:13 +0530

    09: Agents Setup

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,21 +1,7 @@
 "use client";
 
-import { useTRPC } from "@/trpc/client";
-import { useQuery } from "@tanstack/react-query";
-
 const Page = () => {
-  const trpc = useTRPC();
-  const { data, isLoading, error } = useQuery(trpc.hello.queryOptions({ text: "Parth" }));
-
-  if (isLoading) {
-    return <div className="text-white">Loading...</div>;
-  }
-
-  if (error) {
-    return <div className="text-white">Error: {error.message}</div>;
-  }
-
-  return <div className="text-white">{data?.greeting}</div>;
+  return <div className="text-white">Home View</div>;
 };
 
 export default Page;
