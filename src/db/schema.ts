7b0e331e7aecc489fb54d72e134c52d16e8907c8
Parth Chaturvedi <parthchaturvedi0204@gmail.com>
2026-01-29 15:10:27 +0530

    16: Meetings Setup

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,7 +1,7 @@
 
 import { nanoid } from "nanoid";
 
-import { pgTable, text, timestamp, boolean, index } from "drizzle-orm/pg-core";
+import { pgTable, text, timestamp, boolean, index, pgEnum } from "drizzle-orm/pg-core";
 
 export const user = pgTable("user", {
   id: text("id").primaryKey(),
@@ -80,6 +80,32 @@ export const agents = pgTable("agents", {
   name: text("name").notNull(),
   userId: text("user_id").notNull().references(() => user.id, { onDelete: "cascade" }),
   instructions: text("instructions").notNull(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at")
+    .defaultNow()
+    .$onUpdate(() => /* @__PURE__ */ new Date())
+    .notNull(),
+})
+
+export const meetingStatus = pgEnum("meeting_status", [
+  "upcoming",
+  "active",
+  "completed",
+  "processing",
+  "canceled"
+]);
+
+export const meetings = pgTable("meetings", {
+  id: text("id").primaryKey().$default(() => nanoid()),
+  name: text("name").notNull(),
+  userId: text("user_id").notNull().references(() => user.id, { onDelete: "cascade" }),
+  agentId: text("agent_id").notNull().references(() => agents.id, { onDelete: "cascade" }),
+  status: meetingStatus("status").default("upcoming"),
+  startedAt: timestamp("started_at"),
+  endedAt: timestamp("ended_at"),
+  transcriptUrl: text("transcript_url"),
+  recordingUrl: text("recording_url"),
+  summary: text("summary"),
   createdAt: timestamp("created_at").defaultNow().notNull(),
   updatedAt: timestamp("updated_at")
     .defaultNow()
---
+++ src/modules/meetings/ui/view/meetings-view.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { ErrorState } from "@/components/alert-state";
+import { LoadingState } from "@/components/loading-state";
+import { useTRPC } from "@/trpc/client";
+import { useSuspenseQuery } from "@tanstack/react-query";
+
+export const MeetingsView = () => {
+  const trpc = useTRPC();
+  const { data } = useSuspenseQuery(trpc.meetings.getMany.queryOptions({}));
+
+  return <div>{JSON.stringify(data?.items)}</div>;
+};
+
+export const MeetingsViewLoading = () => {
+  return (
+    <LoadingState
+      title="Loading Meetings"
+      description="Please wait while we fetch the Meetings."
+    />
+  );
+};
+
+export const MeetingsViewError = () => {
+  return (
+    <ErrorState
+      title="Error Loading Meetings"
+      description="Failed to load Meetings. Please try again later."
+    />
+  );
+};
